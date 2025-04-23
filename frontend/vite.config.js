e185034b956af6239d943914c90ce0fafe8c5689
ChadarasipalliYaswanth <yaswanthchadarasipalli@gmail.com>
2025-04-23 15:17:03 +0530

    modified vite again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 4173,
+    port: process.env.PORT || 5000,
     host: true,
     allowedHosts: ['smartlearn-nct9.onrender.com']
   }
